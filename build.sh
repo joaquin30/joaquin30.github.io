8225fe24ba92cb4c8f600eb368e359f5dc49dbe6
Joaquín Pino <jpinoz@pm.me>
2021-01-05 21:28:59 -0500

    mar 05 ene 2021 21:28:59 -05

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 header=$(cat header.txt)
-header+=$"\n"
-footer=$"</body>\n</html>"
+footer=$"\n</body>\n</html>"
 for file in md/*
 do
 	name=${file:12}
 	name=$(basename "$name" ".md")
 	aux=$(echo "$name" | sed -e 's/\(.*\)/\L\1/')
 	aux=$(echo "$aux" | sed -e 's/\s/_/g')
-	footer=$(printf "<a href=\"${aux}.html\">${name}</a><br>\n${footer}")
+	footer=$(printf "\n<a href=\"${aux}.html\">${name}</a><br>${footer}")
 	pandoc -s "$file" -o "${aux}.html"
 done
 
