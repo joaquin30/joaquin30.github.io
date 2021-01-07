07277e480a1b8815416228890a0a730e49a24782
Joaquín Pino <jpinoz@pm.me>
2021-01-06 23:39:16 -0500

    mié 06 ene 2021 23:39:16 -05

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ do
 	aux=$(echo "$name" | sed -e 's/\(.*\)/\L\1/')
 	aux=$(echo "$aux" | sed -e 's/\s/_/g')
 	footer=$(printf "<a href=\"post/${aux}.html\">${name}</a><br>\n${footer}")
-	pandoc -s --css=styling.css -V lang=es -V highlighting-css= --mathjax --to=html5 "$file" -o "post/${aux}.html"
+	pandoc -s --css=../styling.css -V lang=es -V highlighting-css= --mathjax --to=html5 "$file" -o "post/${aux}.html"
 done
 
 header+=$footer
