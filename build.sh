d4d3f503af1633af37bb771e716165b1a9869cc9
Joaquín Pino <jpinoz@pm.me>
2021-01-12 16:45:30 -0500

    mar 12 ene 2021 16:45:30 -05

--- build.sh
+++ build.sh
@@ -10,21 +10,28 @@ done
 
 # crear los archivos .html
 rm post/*.html
-header=$(pandoc -s --css=style.css -V lang=es -V highlighting-css= --mathjax --to=html5 src/header.md)
-header=${header::-15}
-footer=$"</body>\n</html>"
+html_header=$(pandoc -s --css=style.css -V lang=es -V highlighting-css= --mathjax --to=html5 src/header.md)
+html_header=${html_header::-15}
+html_footer=$(printf "</body>\n</html>")
+rss_header=$(printf "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">\n<channel>\n  <title>La cloroteca</title>\n  <link>https://joaquin30.github.io</link>\n  <description>Críticas sobre películas de Claro Video</description>\n")
+rss_footer=$(printf "</channel>\n</rss>")
+
 for file in src/md/*
 do
 	name=${file:16}
 	name=$(basename "$name" ".md")
 	aux=$(echo "$name" | sed -e 's/\(.*\)/\L\1/')
 	aux=$(echo "$aux" | sed -e 's/\s/_/g')
-	footer=$(printf "<a href=\"post/${aux}.html\">${name}</a><br>\n${footer}")
+	html_footer=$(printf "<a href=\"post/${aux}.html\">${name}</a><br>\n${html_footer}")
+	rss_footer=$(printf "  <item>\n    <title>${name}</title>\n    <link>https://joaquin30.github.io/post/${aux}</link>\n    <description>Crítica de la película \"${name}\"</description>\n  </item>\n${rss_footer}")
 	pandoc -s --css=../style.css -V lang=es -V highlighting-css= --mathjax --to=html5 "$file" -o "post/${aux}.html"
 done
 
-header+=$footer
-echo "$header" > index.html
+html_header+=$html_footer
+echo "$html_header" > index.html
+rss_header+=$rss_footer
+echo "$rss_header" > feed.rss
+
 
 # subir el sitio web
 git add --all
--- src/md/21-01-03 Europa Report.md
+++ src/md/21-01-03 Europa Report.md
@@ -9,7 +9,7 @@ author: 3 de enero de 2021
 
 Siempre me gustaron las películas de exploración espacial, y esta no es la excepción. *Informe Europa* es una oda espacial, pero más que todo, una busqueda de vida, con tintes de terror y suspenso.
 
-La ambientación es buena, en especial del satélite Europa, aunque existen pequeños errores de *CGI*.
+La ambientación es buena en especial del satélite Europa, aunque existen pequeños errores de CGI.
 Las actuaciones tambien son buenas, pero lo que más destaca en la película es la historia y como es presentada.
 Está en formato documental, y creo que fue un acierto, ya que en los primeros minutos vemos la muerte de un astronauta pero no sabremos como ocurrió hasta avanzar tres cuartos del largometraje.
 
