6efc04064a9d83aabc8f625f2a07b2ecea9dfec3
Joaquín Pino <jpinoz@pm.me>
2021-01-25 15:46:49 -0500

    lun 25 ene 2021 15:46:49 -05

--- build.sh
+++ build.sh
@@ -5,15 +5,14 @@ do
 	name=$(basename "$name" ".png")
 	name=$(basename "$name" ".jpg")
 	name=$(basename "$name" ".jpeg")
-	convert "$file" -quality 50 -resize 480 "img/${name}.webp"
+	convert "$file" -resize 480 "img/${name}.webp"
 done
 
 rm post/*.html
-html_header=$(pandoc -s --css=style.css -V lang=es -V highlighting-css= --mathjax --to=html5 src/header.md)
-html_header=${html_header::-15}
+html_header=$(cat src/header.html)
 html_footer=$(printf "</ul>\n</body>\n</html>")
-rss_header=$(printf "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">\n<channel>\n  <title>La cloroteca</title>\n  <link>https://joaquin30.github.io</link>\n  <description>Críticas sobre películas de Claro Video</description>\n")
-rss_footer=$(printf "</channel>\n</rss>")
+rss_header=$(printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss version=\"2.0\">\n<channel>\n<title>La cloroteca</title>\n<link>https://joaquin30.github.io</link>\n<description>Críticas sobre películas de Claro Video</description>")
+rss_footer="</channel>\n</rss>"
 
 for file in src/md/*
 do
@@ -22,11 +21,10 @@ do
 	aux=$(echo "$name" | sed -e 's/\(.*\)/\L\1/')
 	aux=$(echo "$aux" | sed -e 's/\s/_/g')
 	html_footer=$(printf "<li><a href=\"post/${aux}.html\">${name}</a></li>\n${html_footer}")
-	rss_footer=$(printf "  <item>\n    <title>${name}</title>\n    <link>https://joaquin30.github.io/post/${aux}</link>\n    <description>Crítica de la película \"${name}\"</description>\n  </item>\n${rss_footer}")
-	pandoc -s --css=../style.css -V lang=es -V highlighting-css= --mathjax --to=html5 "$file" -o "post/${aux}.html"
+	rss_footer=$(printf "<item>\n<title>${name}</title>\n<link>https://joaquin30.github.io/post/${aux}.html</link>\n<description>Crítica de la película \"${name}\"</description>\n</item>\n${rss_footer}")
+	pandoc --template=src/template.html5 "$file" -o "post/${aux}.html"
 done
 
-html_header+="<ul>"
 html_header+=$html_footer
 echo "$html_header" > index.html
 rss_header+=$rss_footer
@@ -34,4 +32,3 @@ echo "$rss_header" > rss.xml
 git add --all
 git commit -m "`date`"
 git push -u origin main 
-
--- src/md/20-12-30 Amores Perros.md
+++ src/md/20-12-30 Amores Perros.md
@@ -1,11 +1,14 @@
 ---
 title: Amores Perros
-author: 30 de diciembre de 2020
 ---
+
+# &starf;&starf;&starf;&starf;&starf;
+
+### 30 de diciembre de 2020
 
 ![](../img/amores_perros.webp "Poster de la película")
 
-### Advertencia de *spoilers*
+*Advertencia de *spoilers**
 
 Esta película te sumerje en la vida de 3 personas, quienes literalmente los perros conectaran sus vidas, y a pesar de ser tan distintos todos sufren del mal del amor.
 De por si la película es muy cruda, en especial la primera hora con las peleas de perros, pero me pareció necesario para representar el mundo de los protagonistas.
@@ -15,7 +18,5 @@ Las actuaciones son muy buenas, pero para mi destaca el último personaje, ya qu
 Existen algunas cosas que le restan puntos a la película, pero son minucias, como el ritmo variable, ya que en el segundo tercio las cosas se tornan lentas.
 
 No es un largometraje para ver con la familia, pero estoy seguro que los que la vean se sentirán identificados de alguna forma, ya que todos sufrimos o sufriremos por amor. 
-
-### Calificación: 9/10
 
 [**Volver al inicio**](../index.html)
